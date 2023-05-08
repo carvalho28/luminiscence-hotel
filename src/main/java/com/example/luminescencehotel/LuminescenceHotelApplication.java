47bf2ac189d5fb07b43e5e64130bda9337c75869
carvalho28 <carvalhoo287@gmail.com>
2023-05-08 17:25:17 +0100

    First commit

---
+++ src/main/java/com/example/luminescencehotel/LuminescenceHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.luminescencehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminescenceHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuminescenceHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/luminescencehotel/LuminescenceHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luminescencehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminescenceHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
